7d1211dd8881a3a188af57444077251fef2052ee
jade <jadealvares@hotmail.co.uk>
2018-10-31 13:15:21 +0000

    can touch in, out and check to see if in journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,7 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @touch_in = false
   end
 
   def top_up(amount)
@@ -15,6 +16,18 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @touch_in
+  end
+
+  def touch_in
+    @touch_in = true
+  end
+
+  def touch_out
+    @touch_in = false
   end
 
 end
