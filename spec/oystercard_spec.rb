39199d372f33482cfc5ca9b94e4e269b0a77afbf
jade <jadealvares@hotmail.co.uk>
2018-10-31 12:19:53 +0000

    can now deduct money from balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,4 +20,14 @@ describe Oystercard do
       expect{ subject.top_up 1 }.to raise_error 'max balance of #{max_balance} exceeded'
     end
   end
+
+  describe '#deduct' do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'can deduct money to balance' do
+      subject.top_up(20)
+      expect{ subject.deduct 5}.to change{ subject.balance }.by -5
+    end
+  end
 end
